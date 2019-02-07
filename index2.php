2fedfbbd392c77ee3cd07415def86680dc77b13d
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-02-07 08:15:10 -0500

    Takes in input and appends for Dark Sky API

--- index2.php
+++ index2.php
@@ -3,8 +3,11 @@
 
 $nowInPHP = $_GET["theData"];
 // $nowInPHP = "2";
-$baseURL = "https://superheroapi.com/api.php/10218406907863622/";
-$baseURL = "https://api.darksky.net/forecast/33f04cae19d269f690b892deaa90f918/37.8267,-122.4233";
+// $baseURL = "https://superheroapi.com/api.php/10218406907863622/";
+// $baseURL = "https://api.darksky.net/forecast/33f04cae19d269f690b892deaa90f918/37.8267,-122.4233";
+//Use 37.8267,-122.4233" as input
+$baseURL = "https://api.darksky.net/forecast/33f04cae19d269f690b892deaa90f918/";
+
 
 $combo = $baseURL . $nowInPHP;
 echo 'The combo is ' . $combo;
